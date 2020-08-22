56dcfd27756aec8f4d36ffb6cf3455add88466ca
junyam1020 <junyam7489@gmail.com>
2020-08-22 14:48:38 +0900

    Revert "Issue6"
    This reverts commit e9607e792e1fb13dcdfc235da94a7f7de9ae4604.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,13 +34,8 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = Date.today.wday + x
-      #もしもwday_numが7以上であれば、7を引く
-      if wday_num >= 7
-        wday_num = wday_num - 7
-      end
       # days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, weed_day_char: (wdays[wday_num])}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
   end
